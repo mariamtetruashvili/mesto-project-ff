fdb69d56619d2389c73fd158b13b4d7b2b790c25
Mariam Tetruashvili <maritetruashvili17@gmail.com>
2025-04-05 18:53:23 +0300

    Project update

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function createCard(cardData, deleteCallback) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    deleteButton.addEventListener("click", () => {
+        deleteCallback(cardElement);
+    });
+    return cardElement;
+}
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+const cardsContainer = document.querySelector(".places__list");
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    cardsContainer.append(cardElement);
+});
